423efe6c221ec5a5f7ecf7d441d90649f16684e1
hanqiongding <520156723@qq.com>
2021-06-27 22:01:50 +0800

    fixed: 优化查询分享详情接口的硬编码

--- spring-boot-content/src/main/java/per/hqd/contentcenter/service/content/ShareService.java
+++ spring-boot-content/src/main/java/per/hqd/contentcenter/service/content/ShareService.java
@@ -1,8 +1,10 @@
 package per.hqd.contentcenter.service.content;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,18 +13,29 @@ import per.hqd.contentcenter.domain.dto.content.ShareDTO;
 import per.hqd.contentcenter.domain.dto.user.UserDTO;
 import per.hqd.contentcenter.domain.entity.content.Share;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShareService {
 
     private final ShareMapper shareMapper;
+
     private final RestTemplate restTemplate;
+
+    private final DiscoveryClient discoveryClient;
 
     public ShareDTO findById(Integer id) {
         Share share = this.shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
+        //获取user-center服务的实例
+        String targetURL = this.discoveryClient.getInstances("user-center")
+                .stream()
+                .map(instance -> instance.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前user-center服务没有实例"));
+        log.info("请求的目标地址是：{}", targetURL);
         UserDTO userDTO = this.restTemplate.getForObject(
-                "http://localhost:8080/users/{id}",
+                targetURL,
                 UserDTO.class,
                 userId
         );
