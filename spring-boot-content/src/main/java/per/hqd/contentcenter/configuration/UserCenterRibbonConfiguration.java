4254beb53ec03a2c5823a9270e01adcb04d44389
hanqiongding <520156723@qq.com>
2021-06-30 00:02:05 +0800

    add: ribbon细粒度负载均衡，全局配置，代码实现最佳方式

--- spring-boot-content/src/main/java/per/hqd/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ spring-boot-content/src/main/java/per/hqd/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,6 +1,10 @@
 package per.hqd.contentcenter.configuration;
 
-//@Configuration
-//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)//负载均衡哪个服务名
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.context.annotation.Configuration;
+import per.hqd.ribbonConfiguration.RibbonConfiguration;
+
+@Configuration
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)//负载均衡哪个服务名
 public class UserCenterRibbonConfiguration {
 }
--- spring-boot-content/src/main/java/per/hqd/ribbonConfiguration/RibbonConfiguration.java
+++ spring-boot-content/src/main/java/per/hqd/ribbonConfiguration/RibbonConfiguration.java
@@ -1,11 +1,16 @@
 package per.hqd.ribbonConfiguration;
 
-//@Configuration//该注解用于定义配置类，可替换xml配置文件，该注解下还会有多个@Bean，初始化spring容器
-////即 把xml中的<Bean>生成方式 改成 注解的生成方式
-//public class RibbonConfiguration {//该类位于springboot启动类同级之外的目的是，防止上下文重叠
-//
-//    @Bean// ribbonRule 相当于 <Bean id>
-//    public IRule ribbonRule(){
-//        return new RandomRule();
-//    }
-//}
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration//该注解用于定义配置类，可替换xml配置文件，该注解下还会有多个@Bean，初始化spring容器
+//即 把xml中的<Bean>生成方式 改成 注解的生成方式
+public class RibbonConfiguration {//该类位于springboot启动类同级之外的目的是，防止上下文重叠
+
+    @Bean// ribbonRule 相当于 <Bean id>
+    public IRule ribbonRule(){
+        return new RandomRule();
+    }
+}
