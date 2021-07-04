b646ef1620a892b06cc92fca5f7f6b4d637d3a18
hanqiongding <520156723@qq.com>
2021-07-04 15:05:52 +0800

    modify: [Feign] 用配置文件添加Feign日志

--- spring-boot-content/src/main/java/per/hqd/contentcenter/feignClient/UserCenterFeignClient.java
+++ spring-boot-content/src/main/java/per/hqd/contentcenter/feignClient/UserCenterFeignClient.java
@@ -3,13 +3,13 @@ package per.hqd.contentcenter.feignClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import per.hqd.contentcenter.configuration.UserCenterFeignConfiguration;
 import per.hqd.contentcenter.domain.dto.user.UserDTO;
 
 /**
  * 请求user-center服务的实例都通过这个类的方法去调用
  */
-@FeignClient(name = "user-center", configuration = UserCenterFeignConfiguration.class)
+//@FeignClient(name = "user-center", configuration = UserCenterFeignConfiguration.class)
+@FeignClient(name = "user-center")
 public interface UserCenterFeignClient {
 
     /**
