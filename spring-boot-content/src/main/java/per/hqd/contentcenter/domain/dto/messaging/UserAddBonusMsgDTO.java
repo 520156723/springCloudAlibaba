304571924a4541ffbe810a2f0d8f2734f9e1d073
hanqiongding <520156723@qq.com>
2021-07-15 00:03:59 +0800

    add[MQ]: 生产者编写

---
+++ spring-boot-content/src/main/java/per/hqd/contentcenter/domain/dto/messaging/UserAddBonusMsgDTO.java
@@ -0,0 +1,26 @@
+package per.hqd.contentcenter.domain.dto.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 定义一个消息体
+ * 用户中心获得该类消息要给用户加积分
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserAddBonusMsgDTO {
+    /**
+     * 为谁加积分
+     */
+    private Integer userId;
+
+    /**
+     * 加多少积分
+     */
+    private Integer bonus;
+}
--- spring-boot-content/src/main/java/per/hqd/contentcenter/service/content/ShareService.java
+++ spring-boot-content/src/main/java/per/hqd/contentcenter/service/content/ShareService.java
@@ -2,6 +2,7 @@ package per.hqd.contentcenter.service.content;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import per.hqd.contentcenter.dao.content.ShareMapper;
 import per.hqd.contentcenter.domain.dto.content.ShareAuditDTO;
 import per.hqd.contentcenter.domain.dto.content.ShareDTO;
+import per.hqd.contentcenter.domain.dto.messaging.UserAddBonusMsgDTO;
 import per.hqd.contentcenter.domain.dto.user.UserDTO;
 import per.hqd.contentcenter.domain.entity.content.Share;
 import per.hqd.contentcenter.feignClient.UserCenterFeignClient;
@@ -25,6 +27,8 @@ public class ShareService {
 
     private final UserCenterFeignClient userCenterFeignClient;
 
+    private final RocketMQTemplate rocketMQTemplate;
+
     public ShareDTO findById(Integer id) {
         Share share = this.shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
@@ -36,19 +40,27 @@ public class ShareService {
         return shareDTO;
     }
 
-    public Share auditById(Integer id, ShareAuditDTO auditDTO){
+    public Share auditById(Integer id, ShareAuditDTO auditDTO) {
         Share share = this.shareMapper.selectByPrimaryKey(id);
         if (share == null) {
             throw new IllegalArgumentException("参数非法！该分享不存在");
         }
         //只有未审核状态才能审核
-        if(!Objects.equals(share.getAuditStatus(), "NOT_YET")){
+        if (!Objects.equals(share.getAuditStatus(), "NOT_YET")) {
             throw new IllegalArgumentException("该分享已经审核");
         }
         //修改审核状态
         share.setAuditStatus(auditDTO.getAuditStatusEnum().toString());
         this.shareMapper.updateByPrimaryKey(share);
-        //todo 给发布人加积分
+        if ("PASS".equals(auditDTO.getAuditStatusEnum().toString())) {
+            UserAddBonusMsgDTO msg = UserAddBonusMsgDTO.builder()
+                    .userId(share.getUserId())
+                    .bonus(50)
+                    .build();
+            //如果通过审核，把消息发送到消息队列，让用户中心消费，并且加积分。发给add-bonus，第二个参数是消息体
+            rocketMQTemplate.convertAndSend("add-bonus", msg);
+            log.info("发送消息{}给主题add-bonus", msg.toString());
+        }
         return share;
     }
 
